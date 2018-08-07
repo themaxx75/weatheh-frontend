603e6203f17649a9a865b23350cb26f524f1060e
Maxime Lapointe <maxx@themaxx.ca>
2018-08-07 18:24:46 -0400

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -233,8 +233,8 @@
           </v-layout>
 
           <!-- Hourly -->
-          <v-card flat class="grey lighten-5">
-            <v-card-text class="overflow-hidden py-0">
+          <v-card flat class="grey lighten-5" style="-webkit-overflow-scrolling: touch;">
+            <v-card-text class="overflow-hidden py-0" style="-webkit-overflow-scrolling: touch;">
               <v-layout row align-content-center class="horiz-scroll" style="-webkit-overflow-scrolling: touch;">
                 <v-flex
                   v-for="hourly in forecastResults['weather']['hourly']"
